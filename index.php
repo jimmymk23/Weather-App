316d40ea2244be2f1614ada48ed09a8ce2238806
jimmymk23 <jimkeseling@gmail.com>
2020-04-03 19:31:21 -0400

    Basic Files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="James Keseling">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <p><span id="location"></span></p>
+    <p><span id="currentTemp"></span></p>
+    <p><span id="highTemp"></span></p>
+    <p><span id="lowTemp"></span></p>
+    <p><span id=""></span></p>
+    <p><span id=""></span></p>
+    <p><span id=""></span></p>
+    <p><span id=""></span></p>
+    <p><span id=""></span></p>
+
+    <script defer src="weather.js"></script>
+</body>
+</html>
