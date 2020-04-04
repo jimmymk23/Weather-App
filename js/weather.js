67fe18803921591bcfe242d2eade7fb57af1e1a2
jimmymk23 <jimkeseling@gmail.com>
2020-04-04 16:10:36 -0400

    Find location and generate link
    Able to get the user's current position in coordinates and save to sessionStorage. Coordinates then used to produce weather api link

--- js/weather.js
+++ js/weather.js
@@ -1,28 +1,34 @@
 const api_url = "http://api.openweathermap.org/data/2.5/forecast?";
 const api_key = "6fc5719319c59bba39b02ad8f550424f";
 let api_link;
-let lat;
-let lon;
+let user_location;
+let coords;
+
 
 getLocation();
 
+coords = JSON.parse(sessionStorage.getItem("current_location"));
+
+api_link = updateAPILink(coords.lat, coords.lon);
+
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(locationSuccess);
-    }
-}
+        navigator.geolocation.getCurrentPosition((position) => {
 
-function locationSuccess(position) {
-    document.getElementById('city').innerHTML = position.coords.latitude;
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    updateAPILink(lat, lon);
+            let x = position.coords.latitude;
+            let y = position.coords.longitude;
+            let user_location = {
+                lat: x,
+                lon: y
+            }
+            sessionStorage.setItem("current_location", JSON.stringify(user_location));
+        });
+    }
 }
 
 function updateAPILink(x, y) {
     api_link = `${api_url}lat=${x}&lon=${y}&APPID=${api_key}`;
-    console.log(api_link);
+    return api_link;
 }
 
-// api_link = `${api_url}lat=${lat}&lon=${lon}&APPID=${api_key}`;
 console.log(api_link);
