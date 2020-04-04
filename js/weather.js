56dff86a5317bc9cbf10d8c4931b6cf468ee0b77
jimmymk23 <jimkeseling@gmail.com>
2020-04-03 23:43:55 -0400

    Trying to make it work :)

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,28 @@
 const api_url = "http://api.openweathermap.org/data/2.5/forecast?";
-const api_key = "&APPID=6fc5719319c59bba39b02ad8f550424f";
-
-const api_link = api_url + "zip=47403" + api_key;
-console.log(api_link);
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(setCoordinates);
-    } else {
-        console.log("Could not find location.")
-    }
-}
-
-function setCoordinates(location) {
-    var lat = location.coords.latitude;
-    var lon = location.coords.longitude;
-}
+const api_key = "6fc5719319c59bba39b02ad8f550424f";
+let api_link;
+let lat;
+let lon;
 
 getLocation();
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(locationSuccess);
+    }
+}
+
+function locationSuccess(position) {
+    document.getElementById('city').innerHTML = position.coords.latitude;
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    updateAPILink(lat, lon);
+}
+
+function updateAPILink(x, y) {
+    api_link = `${api_url}lat=${x}&lon=${y}&APPID=${api_key}`;
+    console.log(api_link);
+}
+
+// api_link = `${api_url}lat=${lat}&lon=${lon}&APPID=${api_key}`;
+console.log(api_link);
